520cd2dd985e02c7f297b429d46cfd531f8f929c
Łukasz Piątkowski <piontec@gmail.com>
2020-03-31 12:11:41 +0200

    help text

--- kind-app-testing.sh
+++ kind-app-testing.sh
@@ -202,6 +202,22 @@ print_help () {
   echo "                                  but provide your own"
   echo "  -p, --pre-script-file [path]    override the default path to look for the"
   echo "                                  pre-test hook script file"
+  echo ""
+  echo "Requirements: kind, helm, pipenv."
+  echo ""
+  echo "This script builds and tests a helm chart using a kind cluster. The only required"
+  echo "parameter is [chart name], which needs to be a name of the chart and also a directory"
+  echo "name in the \"helm/\" directory. If there are YAML files present in the directory"
+  echo "helm/\[chart name\]/ci\", a full test starting with creation of a new clean cluster"
+  echo "will be executed for each one of them". 
+  echo "If there's a file \"helm/[chart name]/si/pre-test-hook.sh\", it will be executed after"
+  echo "the cluster is ready to deploy the tested application, but before the application"
+  echo "is deployed. KUBECONFIG variable is set to the test cluster for the script execution."
+  echo "In the next step the chart is built, pushed to the chart repository in the cluster"
+  echo "and the App CR is created to deploy the application."
+  echo "The last (and optional) step is to execute functional test. If the directory"
+  echo "\"test/kind\" is present in the top level directory, the command \"pipenv exec pytest\""
+  echo "is executed as the last step."
 }
 
 ##################
